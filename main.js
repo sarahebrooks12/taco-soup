9fb1d0cf5eff7a6aea1a3f6f53ee8cd80e0e64c0
sarahebrooks12 <sarahebrooks12@gmail.com>
2023-02-15 11:17:35 -0500

    added student grades exercise

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ var harryPotterTitles = ["and the Sorcerer's Stone", "and the Chamber of Secrets
 for (let i = 0; i < harryPotterTitles.length; i++){
     console.log(`Harry Potter ${harryPotterTitles[i]}`)
 }
+
+// Student Grades
+// Copy and paste the folowing array of grades into your JavaScript file:
+var grades = [92, 91, 75, 66, 52, 90, 83, 85, 64, 90, 72, 88, 77, 98, 100, 73, 92]
+// Loop through the grades with a for loop
+// If the current grade is greater than or equal to 0 and less than or equal to 69, log "You got an F" to the console.
+// If it's between 70 and 76, log "You got a D" to the console.
+// If it's between 77 and 84, log "You got a C" to the console.
+// If it's between 84 and 92, log "You got a B" to the console.
+// If it's between 93 and 100, log "You got an A" to the console.
+// Student Grades: Part 2
+// Utilizing the array from the student grades exercise above, each entry in the array represents a different student's percentage grade for the entire course:
+
+// Use JavaScript to answer the following questions:
+
+// How many students got A's?
+// How many students got B's?
+// Which was the most common letter grade?
+// What was the average percentage grade in the class?
