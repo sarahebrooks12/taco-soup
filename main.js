ee8c20f54d96671fa97edd53baa838549065bdc6
sarahebrooks12 <sarahebrooks12@gmail.com>
2023-02-15 11:13:37 -0500

    finished HP exercise taco soup

---
+++ main.js
@@ -0,0 +1,14 @@
+var harryPotterTitles = ["and the Sorcerer's Stone", "and the Chamber of Secrets", "and the Prisoner of Azkaban", "and the Goblet of Fire", "and the Order of the Phoenix", "and the Half-Blood Prince", "and the Deathly Hallows"];
+
+// Use a for loop to output the complete titles to the console.
+// Output:
+// "Harry Potter and the Sorcerer's Stone"
+// "Harry Potter and the Chamber of Secrets"
+// .
+// .
+// .
+// "Harry Potter and the Deathly Hallows"
+
+for (let i = 0; i < harryPotterTitles.length; i++){
+    console.log(`Harry Potter ${harryPotterTitles[i]}`)
+}
